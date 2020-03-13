e291c4adeceaa9cd94cbf76d3e760e4940c09914
doublecouponday <mail@samueljenks.me>
2020-03-13 15:38:31 +1300

    draft:localfile test with isolated tables

--- tests/aggregating.test.js
+++ tests/aggregating.test.js
@@ -143,85 +143,91 @@ describe("when a URL data source is aggregated", () => {
     }, constants.asyncTimeout)
 })
 
-// describe("when a localfile data source is aggregated", () => {
-//     let starters;
-//     const mojPath = `${__dirname}/../exampledata/MOJresponse.json`;
-//     const pcoPath = `${__dirname}/../exampledata/PCOresponse.json`;
+fdescribe("when a localfile data source is aggregated", () => {
+    let starters;
+    let testNames;
+    const mojPath = `${__dirname}/../exampledata/MOJresponse.json`;
+    const pcoPath = `${__dirname}/../exampledata/PCOresponse.json`;
 
-//     beforeAll(async () => {
-//         starters = await helpers.getStartData();
-//     }, constants.asyncTimeout)
+    beforeEach(async () => {
+        starters = await helpers.getStartData();
+        testNames = await helpers.createRandomNames();
+    }, constants.asyncTimeout)
 
-//     async function dothething(entrypoint, chosenfile) {
-//         await runTest(entrypoint, starters.pgPoolConnection, starters.pgPromise, constants.localFileType, chosenfile, null);
-//     }
+    async function dothething(entrypoint, chosenfile, tableName) {
+        await runTest(entrypoint, starters.pgPoolConnection, starters.pgPromise, constants.localFileType, chosenfile, tableName, null);
+    }
 
-//     it('aggregates cases without error', async () => {
-//        await dothething(constants.caseEntryPoint, mojPath);
-//        await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//     }, constants.asyncTimeout)
+    it('aggregates cases without error', async () => {
+        await createCasesTable(starters.pgPoolConnection, testNames.testCases);
+        await dothething(constants.caseEntryPoint, mojPath, testNames.testCases);
+        await helpers.dropTestTable(starters.pgPoolConnection, testNames.testCases);
+    }, constants.asyncTimeout)
 
-//     it('cases can be found in the database', async () => {
-//         await dothething(constants.caseEntryPoint, mojPath);
-//         console.log(`${constants.localFileType} aggregation finished and will be tested...`);
-//         await helpers.checkTableHasResults(starters.pgPoolConnection, constants.casesName);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//     }, constants.asyncTimeout)
+    it('cases can be found in the database', async () => {
+        await createCasesTable(starters.pgPoolConnection, testNames.testCases);
+        await dothething(constants.caseEntryPoint, mojPath, testNames.testCases);
+        console.log(`${constants.localFileType} aggregation finished and will be tested...`);
+        await helpers.checkTableHasResults(starters.pgPoolConnection, testNames.testCases);
+        await helpers.dropTestTable(starters.pgPoolConnection, testNames.testCases);
+    }, constants.asyncTimeout)
 
-//     it('aggregates legislation without error', async () => {
-//         await dothething(constants.legislationEntryPoint, pcoPath);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.legislationName);
-//     }, constants.asyncTimeout)
+    it('aggregates legislation without error', async () => {
+        await createlegislationTable(starters.pgPoolConnection, testNames.testLegislation);
+        await dothething(constants.legislationEntryPoint, pcoPath, testNames.testLegislation);
+        await helpers.dropTestTable(starters.pgPoolConnection, testNames.testLegislation);
+    }, constants.asyncTimeout)
 
-//     it('legislation can be found in the database', async () => {
-//         await dothething(constants.legislationEntryPoint, pcoPath);
-//         console.log(`${constants.localFileType} aggregation finished and will be tested...`);
-//         await helpers.checkTableHasResults(starters.pgPoolConnection, constants.legislationName);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.legislationName);
-//     }, constants.asyncTimeout)
-// })
+    it('legislation can be found in the database', async () => {
+        await createlegislationTable(starters.pgPoolConnection, testNames.testLegislation);
+        await dothething(constants.legislationEntryPoint, pcoPath, testNames.testLegislation);
+        console.log(`${constants.localFileType} aggregation finished and will be tested...`);
+        await helpers.checkTableHasResults(starters.pgPoolConnection, testNames.testLegislation);
+        await helpers.dropTestTable(starters.pgPoolConnection, testNames.testLegislation);
+    }, constants.asyncTimeout)
+})
 
-// describe("when a TT data source is aggregated", () => {
-//     let starters;
-//     const inputPageSize = 1000;
-//     const hugeTimeout = constants.asyncTimeout * 20;
-//     const slowWarning = 'WARNING: PAGINATED TT SOURCE IS VERY SLOW. 20 MIN APPROX.';
+describe("when a TT data source is aggregated", () => {
+    let starters;
+    const inputPageSize = 1000;
+    const hugeTimeout = constants.asyncTimeout * 20;
+    const slowWarning = 'WARNING: PAGINATED TT SOURCE IS VERY SLOW. 20 MIN APPROX.';
 
-//     beforeAll(async () => {
-//         starters = await helpers.getStartData();
-//     }, constants.asyncTimeout)
+    beforeAll(async () => {
+        starters = await helpers.getStartData();
+    }, constants.asyncTimeout)
 
-//     async function doTheThing(entryPoint, batchSize) {
-//         await runTest(entryPoint, starters.pgPoolConnection, starters.pgPromise, constants.TTtype, null, batchSize);
-//     }
+    async function doTheThing(entryPoint, batchSize) {
+        await runTest(entryPoint, starters.pgPoolConnection, starters.pgPromise, constants.TTtype, null, batchSize);
+    }
 
-//     /** WARNING: VERY SLOW. 20MINUTES APPROX */
-//     it('aggregates cases in pages', async () => {
-//         console.log(slowWarning);
-//         await doTheThing(constants.caseEntryPoint, inputPageSize);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//     }, hugeTimeout)
+    /** WARNING: VERY SLOW. 20MINUTES APPROX */
+    it('aggregates cases in pages', async () => {
+        console.log(slowWarning);
+        await doTheThing(constants.caseEntryPoint, inputPageSize);
+        await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
+    }, hugeTimeout)
 
-//     /** WARNING: VERY SLOW. 20MINUTES APPROX */
-//     it('cases can be found in the database after paging', async () => {
-//         console.log(slowWarning);
-//         await doTheThing(constants.caseEntryPoint, inputPageSize);
-//         console.log(`${constants.urlType} aggregation finished and will be tested...`);
-//         await helpers.checkTableHasResults(starters.pgPoolConnection, constants.casesName);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//     }, hugeTimeout)
+    /** WARNING: VERY SLOW. 20MINUTES APPROX */
+    it('cases can be found in the database after paging', async () => {
+        console.log(slowWarning);
+        await doTheThing(constants.caseEntryPoint, inputPageSize);
+        console.log(`${constants.urlType} aggregation finished and will be tested...`);
+        await helpers.checkTableHasResults(starters.pgPoolConnection, constants.casesName);
+        await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
+    }, hugeTimeout)
 
-//     it('aggregates cases', 
-//         async () => {
-//             await doTheThing(constants.caseEntryPoint, null);
-//             await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//         }, constants.asyncTimeout)
+    it('aggregates cases', 
+        async () => {
+            await doTheThing(constants.caseEntryPoint, null);
+            await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
+        }, constants.asyncTimeout)
 
-//     it('cases can be found in the database', async () => {
-//         await doTheThing(constants.caseEntryPoint, null);
-//         console.log(`${constants.urlType} aggregation finished and will be tested...`);
-//         await helpers.checkTableHasResults(starters.pgPoolConnection, constants.casesName);
-//         await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
-//     }, constants.asyncTimeout)
-// })
+    it('cases can be found in the database', async () => {
+        await doTheThing(constants.caseEntryPoint, null);
+        console.log(`${constants.urlType} aggregation finished and will be tested...`);
+        await helpers.checkTableHasResults(starters.pgPoolConnection, constants.casesName);
+        await helpers.dropTestTable(starters.pgPoolConnection, constants.casesName);
+    }, constants.asyncTimeout)
+})
 
